042718c0140bbc3a734df1295377ff7b1fc7b921
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-12 16:49:03 -0500

    Slave userdata.sh to run on slave nodes

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIAQVPOG3TTAZPZGA5U"
-  secret_key = "MhEXzg8qcvcnYq+6zG4GpvD9iKcz/hR/1fUCXEAW"
-  token      = "FwoGZXIvYXdzEPf//////////wEaDCPY6uWPHVc8tz8l2CLEAV9JcvLBrcsEPIHBIMeDrZdo7irA+kPi5NSpbmz34VChKakgA1G+e8IpRYvh/L631wIiT4TABkYqpJSn3HYttpeiDBQ56X+2OYeq3FxqV19uPHcoMFNSiinJJcXrACRm45jwPRIQYhNfa5Q3uPiOts9tRKsCVq6wYhuIv6JbBZzbfXGbGxOuyt9341D4M2/LPWfbSRL/tO90WwjkLT2RkgZ79UFETkwMkCARq3EHruB34rEK81sT23QUKyQGWIg9/H9ml8co6Zy7nAYyLUWkqTMbOUkGD4Ta0LQ9L7JCQQy+O6CUUIFuYI/7nxkTabULN3/Z4gnGhJ178A=="
+  access_key = "ASIAQVPOG3TTIW6AFJDW"
+  secret_key = "oj+0/enIDyR9O8Z588FqOZ2dwOK05rmn6zVurv/s"
+  token      = "FwoGZXIvYXdzEJX//////////wEaDPpTOCtvuZnR1VjjACLEAScJTZc31KRX9TUmIP13orpk0wTyJxabbtDHqgr+yDskh6mBAarwAVWIn0zKh5pKQX6SQoQTRQ0H59cCBCak8tBbSx25THwIvuUo+wWOVxlQj+qvJPmNyP3feY5VViEMSBR4Ir0RGbiOM/sa6EAz5k/hFx3fpyvBHYa7xmpyH9OuiLLVDy3CguvpiFQaxnbMTsF2Pkl4eS/onmgMld5CDQdlkPIEIO3dTXLivY6rCgGPN+9VzH6zavb/PlNPWHKaMu3zoi8ohIfenAYyLe07SwaekZ1RWrQ/rjhoLelHhw6C8b3bq0nyg0zKOAzEipjcha4yQGZVrD/idA=="
 }
 
 resource "aws_security_group" "security_gp" {
@@ -22,8 +22,8 @@ resource "aws_security_group" "security_gp" {
   # verify port
   ingress {
     from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -51,38 +51,39 @@ resource "aws_instance" "instance_micro" {
   user_data              = file("userdata.sh")
   count                  = 1
   tags = {
-    Name = "sql-11"
+    Name = "sql-standalone"
   }
 }
 
-resource "aws_security_group" "cluster_gp" {
-  vpc_id = data.aws_vpc.default.id
+# resource "aws_security_group" "cluster_gp" {
+#   vpc_id = data.aws_vpc.default.id
 
-  # verify port
-  ingress {
-    from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   # verify port
+#   ingress {
+#     from_port        = 0
+#     to_port          = 65535
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
+# }
 
 
 resource "aws_instance" "master" {
   ami                    = "ami-0a6b2839d44d781b2"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.cluster_gp.id]
+  vpc_security_group_ids = [aws_security_group.security_gp.id]
   availability_zone      = "us-east-1c"
   user_data              = file("master.sh")
   count                  = 1
-  private_ip              = "172.35.5.1" # test
+  subnet_id = "subnet-0e56c7e46775aa919" # 172.31.16.0/20
+  private_ip              = "172.31.17.1"
   tags = {
     Name = "sql-master"
   }
@@ -91,11 +92,12 @@ resource "aws_instance" "master" {
 resource "aws_instance" "slave_1" {
   ami                    = "ami-0a6b2839d44d781b2"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.cluster_gp.id]
+  vpc_security_group_ids = [aws_security_group.security_gp.id]
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  private_ip              = "172.33.5.2" # test
+  subnet_id = "subnet-0e56c7e46775aa919" # 172.31.16.0/20
+  private_ip              = "172.31.17.2" 
   tags = {
     Name = "sql-slave-1"
   }
@@ -104,25 +106,27 @@ resource "aws_instance" "slave_1" {
 resource "aws_instance" "slave_2" {
   ami                    = "ami-0a6b2839d44d781b2"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.cluster_gp.id]
+  vpc_security_group_ids = [aws_security_group.security_gp.id]
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  private_ip              = "172.33.5.3" # test
+  subnet_id = "subnet-0e56c7e46775aa919" # 172.31.16.0/20
+  private_ip              = "172.31.17.3"
   tags = {
-    Name = "sql-slave-1"
+    Name = "sql-slave-2"
   }
 }
 
 resource "aws_instance" "slave_3" {
   ami                    = "ami-0a6b2839d44d781b2"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.cluster_gp.id]
+  vpc_security_group_ids = [aws_security_group.security_gp.id]
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  private_ip              = "172.33.5.4" # test
+  subnet_id = "subnet-0e56c7e46775aa919" # 172.31.16.0/20
+  private_ip              = "172.31.17.4"
   tags = {
-    Name = "sql-slave-1"
+    Name = "sql-slave-3"
   }
 }
---
+++ slave.sh
@@ -0,0 +1,42 @@
+echo "HELLOOOOOO"
+sudo apt-get update
+sudo apt-get -y install libclass-methodmaker-perl git dos2unix expect libaio1 libmecab2
+
+sudo mkdir project
+cd project
+
+sudo wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
+sudo dpkg -i mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
+
+# Create my.cnf:
+sudo touch my.cnf
+echo -e -E "[mysql_cluster]
+ndb-connectstring=1172.31.17.1  # Master server" | sudo tee my.cnf
+sudo dos2unix my.cnf
+sudo cp my.cnf /etc/
+
+sudo mkdir -p /usr/local/mysql/data # TODO
+
+sudo touch ndbd.service
+echo -e -E "
+[Unit]
+Description=MySQL NDB Data Node Daemon
+After=network.target auditd.service
+
+[Service]
+Type=forking
+ExecStart=/usr/sbin/ndbd
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee ndbd.service
+
+sudo dos2unix ndbd.service
+sudo cp ndbd.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable ndbd
+sudo systemctl start ndbd
