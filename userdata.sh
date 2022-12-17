d4d5bb510f4a4308bf4c6c6ad155ae1003efb28d
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-17 15:19:39 -0500

    Added echos and removed sakila commands from standalone user data

--- userdata.sh
+++ userdata.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+echo "Installations"
 sudo apt-get update
 sudo apt-get install -y mysql-server sysbench
 
+echo "Dir creation"
 mkdir project
 cd project
 
+echo "Sakila install"
 wget https://downloads.mysql.com/docs/sakila-db.tar.gz
 tar -xvf sakila-db.tar.gz
-
-sudo mysql -u root -e "
-SOURCE ~/../../project/sakila-db/sakila-schema.sql;
-SOURCE ~/../../project/sakila-db/sakila-data.sql;
-"
