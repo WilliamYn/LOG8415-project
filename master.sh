959cb931e24e4f17b749bddc62a33868ea6bf16f
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-17 15:16:45 -0500

    Added ndb_mgmd service to master node with systemctl commands

--- master.sh
+++ master.sh
@@ -57,3 +57,28 @@ NodeId=14" | sudo tee config.ini
 sudo dos2unix config.ini
 sudo mkdir /var/lib/mysql-cluster #TODO
 sudo cp config.ini /var/lib/mysql-cluster/
+
+# service
+sudo touch ndb_mgmd.service
+echo -e -E "[Unit]
+Description=MySQL NDB Cluster Management Server
+After=network.target auditd.service
+
+[Service]
+Type=forking
+ExecStart=/usr/sbin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" | sudo tee ndb_mgmd.service
+sudo dos2unix ndb_mgmd.service
+sudo cp ndb_mgmd.service /etc/systemd/system/
+
+echo "reload"
+sudo systemctl daemon-reload
+echo "enable"
+sudo systemctl enable ndb_mgmd
+echo "start"
+sudo systemctl start ndb_mgmd
