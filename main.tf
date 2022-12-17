4895cce8ed56fd65a0099202766cbd0208975ede
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-17 15:21:08 -0500

    renamed userdata to standalone

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "instance_micro" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.security_gp.id]
   availability_zone      = "us-east-1c"
-  user_data              = file("userdata.sh")
+  user_data              = file("standalone.sh")
   count                  = 1
   tags = {
     Name = "sql-standalone"
