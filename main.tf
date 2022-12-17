261b6003b8b9f8df4d614712e0f9874175718d05
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-17 15:40:31 -0500

    Added proxy userdata shell file

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = "ASIAQVPOG3TTIW6AFJDW"
   secret_key = "oj+0/enIDyR9O8Z588FqOZ2dwOK05rmn6zVurv/s"
   token      = "FwoGZXIvYXdzEJX//////////wEaDPpTOCtvuZnR1VjjACLEAScJTZc31KRX9TUmIP13orpk0wTyJxabbtDHqgr+yDskh6mBAarwAVWIn0zKh5pKQX6SQoQTRQ0H59cCBCak8tBbSx25THwIvuUo+wWOVxlQj+qvJPmNyP3feY5VViEMSBR4Ir0RGbiOM/sa6EAz5k/hFx3fpyvBHYa7xmpyH9OuiLLVDy3CguvpiFQaxnbMTsF2Pkl4eS/onmgMld5CDQdlkPIEIO3dTXLivY6rCgGPN+9VzH6zavb/PlNPWHKaMu3zoi8ohIfenAYyLe07SwaekZ1RWrQ/rjhoLelHhw6C8b3bq0nyg0zKOAzEipjcha4yQGZVrD/idA=="
@@ -65,8 +65,8 @@ resource "aws_instance" "master" {
   availability_zone      = "us-east-1c"
   user_data              = file("master.sh")
   count                  = 1
-  subnet_id = "subnet-0e56c7e46775aa919"
-  private_ip              = "172.31.17.1"
+  subnet_id              = "subnet-0e56c7e46775aa919"
+  private_ip             = "172.31.17.1"
   tags = {
     Name = "sql-master"
   }
@@ -80,8 +80,8 @@ resource "aws_instance" "slave_1" {
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  subnet_id = "subnet-0e56c7e46775aa919"
-  private_ip              = "172.31.17.2" 
+  subnet_id              = "subnet-0e56c7e46775aa919"
+  private_ip             = "172.31.17.2" 
   tags = {
     Name = "sql-slave-1"
   }
@@ -94,8 +94,8 @@ resource "aws_instance" "slave_2" {
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  subnet_id = "subnet-0e56c7e46775aa919"
-  private_ip              = "172.31.17.3"
+  subnet_id              = "subnet-0e56c7e46775aa919"
+  private_ip             = "172.31.17.3"
   tags = {
     Name = "sql-slave-2"
   }
@@ -108,8 +108,8 @@ resource "aws_instance" "slave_3" {
   availability_zone      = "us-east-1c"
   user_data              = file("slave.sh")
   count                  = 1
-  subnet_id = "subnet-0e56c7e46775aa919"
-  private_ip              = "172.31.17.4"
+  subnet_id              = "subnet-0e56c7e46775aa919"
+  private_ip             = "172.31.17.4"
   tags = {
     Name = "sql-slave-3"
   }
@@ -121,8 +121,9 @@ resource "aws_instance" "proxy" {
     instance_type          = "t2.large"
     vpc_security_group_ids = [aws_security_group.security_gp.id]
     availability_zone      = "us-east-1c"
-    subnet_id = "subnet-0e56c7e46775aa919"
-    private_ip              = "172.31.17.5"
+    user_data              = file("proxy.sh")
+    subnet_id              = "subnet-0e56c7e46775aa919"
+    private_ip             = "172.31.17.5"
     tags = {
       Name = "proxy"
     }
