77786a88337495e12af315a827c7785983108248
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-12-20 20:56:36 -0500

    Added ping to find fastest slave node

--- proxy.py
+++ proxy.py
@@ -1,18 +1,26 @@
-
 import subprocess
 
 slave_ip_addresses = ["172.31.17.2", "172.31.17.3", "172.31.17.4"]
-fastest_ping_ip = ""
-fastest_ping_time = 9999
 
 def get_fastest_ping_ip() -> str:
+    fastest_ping_ip = ""
+    fastest_ping_time = 9999
     for ip_address in slave_ip_addresses:
-        output = subprocess.run(["ping", "-c", "1", ip_address], capture_output=True)
-        ping_time = output.stdout.split()[-2].split("/")[1]
-        if ping_time < fastest_ping_time:
-            fastest_ping_time = ping_time
-            fastest_ping_ip = ip_address
+        ping_output = subprocess.run(["ping", "-c", "1", ip_address], capture_output=True)
+        output_list = ping_output.stdout.split()
+        for binary_element in output_list:
+            string_element = binary_element.decode("utf-8")
+            if "time=" in string_element :
+                ping_time = float(string_element.strip("time="))
+                if ping_time < fastest_ping_time:
+                    fastest_ping_time = ping_time
+                    fastest_ping_ip = ip_address
     return fastest_ping_ip
 
+fastest_ping_ip = get_fastest_ping_ip()
+print("Fastest worker is worker", slave_ip_addresses.index(fastest_ping_ip) + 1, "with ip address:", fastest_ping_ip)
 
-print(get_fastest_ping_ip)
+
+"""
+
+"""
